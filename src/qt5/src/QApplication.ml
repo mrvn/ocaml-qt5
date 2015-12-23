ee762e3274a9abc039147717bb85d12549fb4d2b
Goswin von Brederlow <goswin-v-b@web.de>
2015-12-23 17:44:53 +0100

    rename test case to app.ml, fix invocation

--- src/qt5/src/QApplication.ml
+++ src/qt5/src/QApplication.ml
@@ -1,24 +1,27 @@
-  open Ctypes
-  open Foreign
+open Ctypes
+open Foreign
 
-  type t
-  let t : t structure typ = structure "QApplication"
+type t
+let t : t structure typ = structure "QApplication"
 
-  let create =
-    foreign "qApplication_constructor" (void @-> returning (ptr t))
-  let destroy =
-    foreign "qApplication_destructor" (ptr t @-> returning void)
-  let exec =
-    foreign "qApplicaion_exec" (ptr t @-> returning int)
+let create =
+  foreign "qApplication_constructor" (void @-> returning (ptr t))
+let destroy =
+  foreign "qApplication_destructor" (ptr t @-> returning void)
+let exec =
+  foreign "qApplicaion_exec" (ptr t @-> returning int)
 
-  class virtual qObject =
-  object
-  end
+class virtual qObject =
+object
+end
 
-  class qApplication =
-  object
-    inherit qObject
-    val this = create ()
-    method exec = exec this
-    initializer Gc.finalise destroy this
-  end
+class qApplication =
+object
+  inherit qObject
+  val this = create ()
+  method exec = exec this
+  initializer Gc.finalise destroy this
+end
+
+let app = new qApplication
+let () = Printf.printf "foo\n%!"
---
+++ src/test/app.ml
@@ -0,0 +1,2 @@
+let () = Printf.printf "QApplication test\n%!"
+let a = new QT5.QApplication.qApplication
