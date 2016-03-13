b6c08e8250829e2eb821c80eb61dfd6d1637278e
Goswin von Brederlow <goswin-v-b@web.de>
2016-03-13 16:31:51 +0100

    remove dead files

--- src/qt5/src/EventHandler.ml
+++
@@ -1,9 +0,0 @@
-type ('a, 'b) t
-
-external set :
-  'a Proxy.t -> ('a, 'b) t -> 'b -> unit
-    = "caml_mrvn_QT5_EventHandler_set"
-
-class ['a, 'b] eventHandler (proxy : 'a Proxy.t) (handler : ('a, 'b) t) = object
-  method set (fn : 'b) = set proxy handler fn
-end
--- src/qt5/src/Proxy.ml
+++
@@ -1,3 +0,0 @@
-exception ObjectDestroyed
-
-type 'a t
